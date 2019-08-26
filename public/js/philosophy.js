ad21e347e1b13e8909812855ba6911668d86747d
Charles Lee <charleslee90@gmail.com>
2019-08-26 18:05:58 +0800

    Adds philosophy and projects page

---
+++ public/js/philosophy.js
@@ -0,0 +1,24 @@
+import '../css/philosophy.css';
+import {
+  scrollTo,
+  hotReload,
+  sliceArray,
+  mobileNavHandler,
+} from '../helpers/util.js'
+
+/**
+ * Moible Nav Handler
+ */
+// const hamburger = document.querySelector('.hamburger');
+// const mobileNavList = document.querySelector('.mobile-nav-list');
+
+// mobileNavHandler(hamburger, mobileNavList);
+
+/**
+ * Event Listeners
+ */
+
+window.addEventListener('load', function(e) {
+})
+
+hotReload();
---
+++ public/js/projects.js
@@ -0,0 +1,24 @@
+import '../css/projects.css';
+import {
+  scrollTo,
+  hotReload,
+  sliceArray,
+  mobileNavHandler,
+} from '../helpers/util.js'
+
+/**
+ * Moible Nav Handler
+ */
+// const hamburger = document.querySelector('.hamburger');
+// const mobileNavList = document.querySelector('.mobile-nav-list');
+
+// mobileNavHandler(hamburger, mobileNavList);
+
+/**
+ * Event Listeners
+ */
+
+window.addEventListener('load', function(e) {
+})
+
+hotReload();
