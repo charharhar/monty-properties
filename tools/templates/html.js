63418172c2529c98b980f8e4b4ddfc07b282ae13
Charles Lee <charleslee90@gmail.com>
2019-08-27 14:02:19 +0800

    Fixes indentation for template html

--- tools/templates/html.js
+++ tools/templates/html.js
@@ -32,13 +32,13 @@ module.exports =
 </head>
 <body>
 
-  <div class="wrapper">
-  </div>
+    <div class="wrapper">
+    </div>
 
-  <script
-      src="https://code.jquery.com/jquery-3.2.1.min.js"
-      integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-      crossorigin="anonymous"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
 </body>
 </html>
 `
