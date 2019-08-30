fe7315e81256ceddf36476f1aea5f68b162fc505
Charles Lee <charleslee90@gmail.com>
2019-08-30 13:16:38 +0800

    Completes projects page without carousel

--- public/js/projects.js
+++ public/js/projects.js
@@ -17,6 +17,21 @@ import {
 /**
  * Event Listeners
  */
+const modalControl = sliceArray(document.querySelectorAll('.modal-control'))
+
+modalControl.forEach(node => {
+  node.addEventListener('click', e => {
+    const message = node.getAttribute('data-message');
+    const targetId = node.getAttribute('data-target');
+    let className = 'carousel-more-modal ';
+
+    const target = document.querySelector(`#${targetId}`);
+    className += message === 'more' ? 'modal-active' : '';
+
+    console.log(className);
+    target.className = className
+  })
+})
 
 window.addEventListener('load', function(e) {
 })
